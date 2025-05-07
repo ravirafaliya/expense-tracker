d07259d5b67732df52861ef05130f7bfe13d71dc
Ravi Rafaliya <ravirr6997@gmail.com>
2025-05-07 11:54:19 +0530

    setup tailwind css

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,5 +1,16 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="main-container">
+      <div className="header">
+        <div className="header-title">
+          <h1 className="text-3xl font-serif">Expense Tracker</h1>
+        </div>
+        <div className="header-subTitle">
+          <h2>Track your expenses easily</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
